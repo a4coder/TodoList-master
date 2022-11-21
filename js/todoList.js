05b0d491a9304c5735b8408dad3e74588887bb2f
a4coder <eng.alirezashahryari@gmail.com>
2022-11-21 10:53:17 +0330

    delete some codes to start again

--- js/todoList.js
+++ js/todoList.js
@@ -1,54 +1,13 @@
-// identify html elemens
-let todoList_box = document.querySelector("#todoList_box");
 const todoListUrl = "http://localhost:8008/servicebus/run/todoList";
-
-// craete function: just building table rows
-const createTableRow = (arr) => {
-  console.log(arr);
-  let tableRows = arr.map((elem, index) => {
-    return `
-      <tr>
-        <td>${index + 1}</td>
-        <td>${elem.taskTitle}</td>
-        <td>${elem.completed ? "آری" : "خیر"}</td>
-      </tr>`;
-  });
-  return tableRows;
-};
-
-// create function: 1.event handler for #todoList_wrap 2.fetch data to show data in #todoList_wrap
-function fetchData_In_TodoListWrap(arrayOfObject) {
-  console.log(arrayOfObject);
-  todoList_box.addEventListener("click", function () {
-    todoList_box.outerHTML = [
-      `<div id="todoList_box" class="overflowScroll displayBlock">`,
-      `  <table id="todosTable">`,
-      `    <tr>`,
-      `     <th>شماره</th>`,
-      `     <th>عنوان</th>`,
-      `     <th>تکمیل</th>`,
-      `    </tr>`,
-      `    ${createTableRow(arrayOfObject)}`,
-      `  </table>`,
-      `<div>`,
-    ].join("");
-  });
-}
-
-// fetching data and use top functions
-try {
-  async function getResponse() {
-    const response = await fetch(todoListUrl, {
-      method: "POST",
-      headers: {
-        contentType: "application/json; charset=utf-8",
-      },
-    });
-    const data = await response.json();
-    fetchData_In_TodoListWrap(JSON.parse(data));
-  }
-  getResponse();
-} catch (e) {
-  console.log(e);
-}
-
+[
+  `<div id="todoList_box" class="overflowScroll displayBlock">`,
+  `  <table id="todosTable">`,
+  `    <tr>`,
+  `     <th>شماره</th>`,
+  `     <th>عنوان</th>`,
+  `     <th>تکمیل</th>`,
+  `    </tr>`,
+  `    ${createTableRow(arrayOfObject)}`,
+  `  </table>`,
+  `<div>`,
+].join("");
