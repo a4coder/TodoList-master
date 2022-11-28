2b34e5334e055a5421a2c2a960b22ee72cf0fe9e
a4coder <eng.alirezashahryari@gmail.com>
2022-11-28 10:09:49 +0330

    delete db.json

--- model/viewModel.js
+++ model/viewModel.js
@@ -106,9 +106,9 @@ export default class ServerDataViewModel {
 
       Toast.success("ویرایش با موفقیت انجام شد");
       console.log(response);
-      // setTimeout(() => {
-      //   location.reload();
-      // }, 2000);
+      setTimeout(() => {
+        location.reload();
+      }, 2000);
     } catch (error) {
       console.log(error.message);
     }
